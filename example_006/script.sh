9ced6a5cced4d2a277843258de134bbb09236c41
Mert <mertturkmenoglu99@gmail.com>
2019-06-10 03:14:19 +0300

    Add special variables example

---
+++ example_006/script.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Special variables
+# Reading command line arguments
+
+echo "Script name: $0"
+echo "Your username: $USER"
+echo "Total argument count: $#"
+echo "First argument: $1"
+echo "All arguments: $@"
+echo "Time: $SECONDS"
+echo "A random number: $RANDOM"
